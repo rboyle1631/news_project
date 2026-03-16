bb0f3e705dd3f92ae13b524eec234f9fba3132e6
rboyle1631 <rboyle1631@gmail.com>
2026-03-16 18:37:50 -0500

    Add docstrings to views

--- newsapp/views.py
+++ newsapp/views.py
@@ -9,6 +9,19 @@ from .models import Article
 # ---------------------------------------------------------
 @login_required
 def article_list(request):
+    """
+    Display a list of all approved articles.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The incoming HTTP request.
+
+    Returns
+    -------
+    HttpResponse
+        A rendered HTML page showing all approved articles.
+    """
     articles = Article.objects.filter(approved=True)
     return render(request, "newsapp/article_list.html", {"articles": articles})
 
@@ -19,6 +32,21 @@ def article_list(request):
 # ---------------------------------------------------------
 @login_required
 def article_detail(request, article_id):
+    """
+    Display the details of a single article.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The incoming HTTP request.
+    article_id : int
+        The ID of the article to retrieve.
+
+    Returns
+    -------
+    HttpResponse
+        A rendered HTML page showing the article details.
+    """
     article = get_object_or_404(Article, id=article_id)
     return render(request, "newsapp/article_detail.html", {"article": article})
 
@@ -29,6 +57,21 @@ def article_detail(request, article_id):
 # ---------------------------------------------------------
 @permission_required("newsapp.change_article")
 def approve_article(request, article_id):
+    """
+    Approve an article and trigger the approval signal.
+
+    Parameters
+    ----------
+    request : HttpRequest
+        The incoming HTTP request.
+    article_id : int
+        The ID of the article to approve.
+
+    Returns
+    -------
+    HttpResponseRedirect
+        Redirects back to the article detail page after approval.
+    """
     article = get_object_or_404(Article, id=article_id)
     article.approved = True
     article.save()  # This triggers your signal
